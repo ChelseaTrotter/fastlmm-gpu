a1b750baaf19eee91feb07e9d77cbb7291945191
Saunak Sen <sen@uthsc.edu>
2018-02-22 13:10:53 -0600

    Perhaps working code for WLS gpu.

--- src/wls.jl
+++ src/wls.jl
@@ -2,6 +2,8 @@
 # wls: weighted least squares        
 ##################################################################        
 
+import Base.inv
+
 type Wls
     b::Array{Float64,2}
     sigma2::Float64
@@ -65,3 +67,67 @@ function wls(y::Array{Float64,2},X::Array{Float64,2},w::Array{Float64,1},
         
 end
 
+        
+function wls(y::CuArray{Float64,2},X::CuArray{Float64,2},w::CuArray{Float64,1},
+             reml::Bool=false,loglik=false)
+
+    # number of individuals
+    n = size(y,1)
+    # number of covariates
+    p = size(X,2)
+    
+    # check if weights are positive
+    if(any(w.<=.0))
+        error("Some weights are not positive.")
+    end
+         
+    # square root of the weights
+    sqrtw = sqrt.(w)
+    # scale by weights
+    # yy = y.*sqrtw
+    yy = sqrtw.*y
+    # XX = diagm(sqrtw)*X
+    XX = CuArray(Diagonal(sqrtw))*X
+        
+    # solve
+    # XXtXX = XX'*XX
+    (q,r) = qr(XX)
+    b = inv(r)*At_mul_B(q*yy)
+    # estimate yy and calculate rss
+    yyhat = XX*b
+    # yyhat = q*At_mul_B(q,yy)
+    rss = norm((yy-yyhat))^2
+
+    if( reml )        
+        sigma2 = rss/(n-p)
+    else
+        sigma2 = rss/n
+    end
+
+    # return coefficient and variance estimate
+    logdetSigma = n*log(sigma2) - sum(CUDAnative.log.(w)) 
+    ell = -0.5 * ( logdetSigma + rss/sigma2 ) 
+    if ( reml )
+        ell -=  0.5*log(abs(det(r))) - (p/2)*(log(sigma2))
+    end
+        
+    return Wls(b,sigma2,ell)
+        
+end
+
+
+function inv(x::CuArray)
+    return CuArrays.BLAS.matinv_batched([x])[2][1]
+end
+
+
+        using CuArrays
+        n = 100000;
+        p = 100;
+        b = ones(p,1);
+        X = randn(n*2,p);
+        Y = X*b+ randn(n*2,1);
+        W = repeat([4.0; 1.0],inner=n);
+        x = CuArray(X);
+        y = CuArray(Y);
+        w = CuArray(W);
