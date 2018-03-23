668f4ea6f4d91549d39dd3e1e78aa582cbb90f8f
Xiaoqi Hu <xiaoqihu@uthsc.edu>
2018-03-23 17:31:38 +0000

    increased matrix column number for matrix multiplication benchmark

--- test/mm.jl
+++ test/mm.jl
@@ -51,32 +51,31 @@ println("Compare C: ", isapprox(C,h_c; atol = 1e-10))
 
 file = open("benchmark_result.csv", "w")
 for m in [1024,2048,4096,8192,16384]
-    for n in [128, 265, 512, 1024, 2048]
-        if(m>n)
-            println("m = $m, n = $n")
+    for n in [128, 265, 512, 1024, 2048, 4096, 8192, 16384]
+        println("m = $m, n = $n")
 
-            srand(123);
+        srand(123);
 
-            A = randn(m,n)
-            B = randn(m,n)
-            a = CuArray(A)
-            b = CuArray(B)
-            #println("A : ", A)
-            #println("B : ", B)
+        A = randn(m,n)
+        B = randn(m,n)
+        a = CuArray(A)
+        b = CuArray(B)
+        #println("A : ", A)
+        #println("B : ", B)
 
-            tic(); C = A'*B; toc()
-            tic(); c = a'*b; toc()
+        tic(); C = A'*B; toc()
+        tic(); c = a'*b; toc()
 
-            #convert GPU array back to host and check result
-            h_c = convert(Array{Float64,2},c)
-            println("Compare result: ", isapprox(C,h_c; atol = 1e-10))
+        #convert GPU array back to host and check result
+        h_c = convert(Array{Float64,2},c)
+        println("Compare result: ", isapprox(C,h_c; atol = 1e-10))
 
-            #run benchmark
-            cpu_result = benchmark(10, matrix_mult,A,B)
-            gpu_result = benchmark(10, matrix_mult,a,b)
-            write(file, "$m, $n, $(cpu_result[3]),  $(gpu_result[3])\n");
+        #run benchmark
+        cpu_result = benchmark(20, matrix_mult,A,B)
+        gpu_result = benchmark(20, matrix_mult,a,b)
+        speedup = cpu_result[3]/gpu_result[3]
+        write(file, "$m, $n, $(cpu_result[3]),  $(gpu_result[3]), $speedup\n");
 
-        end
     end
 end
 close(file)
